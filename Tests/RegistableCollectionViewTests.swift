7ec94860bf12169a0bde7e421a675e1d515c3e73
devmjun <dev.mjun@gmail.com>
2018-10-09 06:36:50 +0900

    support Carthage

--- Tests/RegistableCollectionViewTests.swift
+++ Tests/RegistableCollectionViewTests.swift
@@ -7,8 +7,7 @@
 //
 
 import XCTest
-@testable import SimpleCell
-
+import SimpleCell
 
 class RegistableCollectionViewTests: XCTestCase {
     var collectionViewController: UICollectionViewController?
