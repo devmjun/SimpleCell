5ce59bdd7cd36da30ac7c037466184f85429e2de
devmjun <dev.mjun@gmail.com>
2018-10-31 23:02:07 +0900

    support header-footer for UITableView

--- Sources/SimpleTableViewHeaderFooter.swift
+++ Sources/SimpleTableViewHeaderFooter.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2018 mjun. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITableViewHeaderFooterView: CellType { }
+
+public extension UITableView {
+    /**
+     Registers a class for use in creating new table header or footer views
+     */
+    func register<HeaderFooter>(headerFooter: HeaderFooter.Type,
+                         forHeaderFooterViewReuseIdentifier reuseIdentifier: String = HeaderFooter.identifier) where HeaderFooter: UITableViewHeaderFooterView {
+        register(headerFooter, forHeaderFooterViewReuseIdentifier: reuseIdentifier)
+    }
+}
+
+public extension UITableView {
+    func dequeue<HeaderFooter>(_ reusableHeaderFooter: HeaderFooter.Type) -> HeaderFooter where HeaderFooter: UITableViewHeaderFooterView {
+        guard let cell = dequeueReusableHeaderFooterView(withIdentifier: reusableHeaderFooter.identifier) as? HeaderFooter else {
+            fatalError("Failed to dequeue a cell with identifier \(reusableHeaderFooter.identifier) matching type \(HeaderFooter.Type.self)")
+        }
+        return cell
+    }
+}
